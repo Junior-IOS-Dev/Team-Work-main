27956fad8c0a29c64a120b978b20d23f6badb975
Никита <nikitapakin33@gmail.com>
2021-11-13 18:30:44 +0300

    Initial Commit

---
+++ Team Work/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Team Work
+//
+//  Created by Maksim  on 13.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var testLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        testLabel.textColor = .blue
+    }
+
+
+}
+
