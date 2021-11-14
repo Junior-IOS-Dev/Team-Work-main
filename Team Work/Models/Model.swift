e7263acb0ff3b0e38f38e2a68c88ffc58f0ef23a
Ruslan Bityukov <bitukov_ruslan@mail.ru>
2021-11-14 20:15:07 +0300

    add data manager, and model person

--- Team Work/Models/Model.swift
+++ Team Work/Models/Model.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+struct Person {
+    
+    let fullName: String
+    let personInfo: String
+    let imageDevoloper: String
+    
+    static func getPerson() -> [Person] {
+        
+        var persons: [Person] = []
+        
+        let fullNames = DataManager.shared.fullNameDeveloper
+        let personsInfo = DataManager.shared.personInfoDeveloper
+        let imageDevolopers = DataManager.shared.imageDeveloper
+        
+        let iterationCount = min(
+            fullNames.count,
+            personsInfo.count,
+            imageDevolopers.count
+        )
+        
+        for index in 0..<iterationCount {
+            let person = Person(fullName: fullNames[index],
+                                personInfo: personsInfo[index],
+                                imageDevoloper: imageDevolopers[index])
+            persons.append(person)
+        }
+        
+        return persons
+    }
+}
+
--- Team Work/Services/DataManager.swift
+++ Team Work/Services/DataManager.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+
+class DataManager {
+    
+    static let shared = DataManager()
+    
+    
+    let fullNameDeveloper = ["Руслан Битюков", "Михаил Позялов", "Maksim Ozhgi", "Чусито"]
+    
+    let personInfoDeveloper = ["", "", ""]
+    
+    let imageDeveloper = ["", "", ""]
+    
+    
+    let bear = ["Pale Ale (Пейл Эль, Бледный Эль)", "Wheat Ales (Пшеничное пиво)", "Belgian Ales (Бельгийские эли)", "Sours (Кисляки)", "Brown Ales (Коричневый эль)", "Porters (Портер)", "Stouts (Стаут)", "Pale Lagers (Светлый лагер)", "Dark Lagers (Темный лагер)", "Bocks (Бок)", "Ambers (Амбер эль)", "Specialty Beer (Специальное пиво)"]
+    
+    let paleAleBear = ["Lagunitas Born Yesterday Pale Ale (США)", "Sierra Nevada Pale Ale (США)", "Dogfish Head 120 Minute IPA (США)"]
+    
+    let wheatAlesBear = ["Weihenstephaner Hefeweissbier (Германия)", "Franziskaner Hefe-Weisse (Германия)", "Samuel Adams Dunkelweizen (CША)"]
+    
+    let belgianAles = ["Orval Trappist Ale (Бельгия)", "Fantôme Saison (Бельгия)", "Westmalle Trappist Tripel (Бельгия)"]
+    
+    let soursBear = ["Goose Island Lolita (США)", "Cuvée Des Jacobins Rouge (Бельгия)", "The Bruery Tart Of Darkness (США)"]
+    
+    let brownAles = ["Bell’s Best Brown (США)", "Samuel Smith’s Nut Brown Ale (Англия)", "Big Sky Moose Drool Brown Ale (США)"]
+    
+    let porters = ["Founders Porter (США)", "Harviestoun Brewery Old Engine Oil (Шотландия)", "Fuller’s London Porter (Англия)"]
+    
+    let stouts = ["Guinness Draught (Ирландия)", "Rogue Double Chocolate Stout (США)", "Deschutes Obsidian Stout (США)"]
+    
+    let paleLagers = ["Jack’s Abby Hoponius Union (США)", "Brasseries Kronenbourg 1664 (Франция)", "Stella Artois (Бельгия)"]
+    
+    let darkLagers = ["Baltika #4 Original (Dark) (Россия)", "Devils Backbone Vienna Lager (США)", "New Belgium 1554 (США)"]
+    
+    
+    let bocks = ["La Trappe Bockbier (Нидерланды)", "Samichlaus Classic Bier (Австрия)", "Spoetzl Shiner Bock (США)"]
+    
+    let ambers = ["New Belgium Fat Tire Amber Ale (США)", "Speakeasy Prohibition Ale (США)", "Bear Republic Red Rocket Ale (США)"]
+    
+    let specialtyBeer = ["Dogfish Head Chateau Jiahu (DE)", "Founders Blushing Monk (MI)", "Left Hand Good Juju (CO)"]
+    
+    let imageBear = [""]
+    
+    private init() {}
+}
